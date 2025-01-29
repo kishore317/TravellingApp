3bd03f4f1ea615373a24f9bb0ba64405d93b54b3
kishore317 <kishorejayaraman02@gmail.com>
2025-01-29 13:02:20 +0530

    Installed expo package

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import { View, Text } from 'react-native';
+import { View, Text } from 'react-native'
+import React from 'react'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>baddu</Text>
+    <View>
+      <Text>App</Text>
     </View>
-  );
+  )
 }
+
+export default App
